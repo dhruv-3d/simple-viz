ccef2e1bf7f7907942ad15a0c8686cbf5ae398d0
Dhruv Desai <dhruvdesai3d@gmail.com>
2021-02-17 03:17:34 +0530

    purav's viz

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from purav import load_data
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 df = load_data()
 
